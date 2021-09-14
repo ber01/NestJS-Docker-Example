56b8b73b6884074af88ca473fd191dd0b01448ee
민경환 <ber01@naver.com>
2021-09-14 11:41:22 +0900

    fix: key값을 name으로 변경

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -17,6 +17,6 @@ export class AppController {
 
   @Get('/name')
   getName(): string {
-    return JSON.stringify({ email: this.appService.getName() })
+    return JSON.stringify({ name: this.appService.getName() })
   }
 }
